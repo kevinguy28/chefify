4c92459dd90b5489c80005e93d75401190213d05
Kevin <101434732+kevinhuytran1128@users.noreply.github.com>
2025-06-17 14:44:20 -0400

    up

--- chefify_backend/chefify_backend/settings.py
+++ chefify_backend/chefify_backend/settings.py
@@ -157,12 +157,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:8000",
-    "https://chefify-production.up.railway.app/",
-] 
+    "https://chefify-production.up.railway.app",
+]
 
 CORS_ALLOW_CREDENTIALS = True
 
-CSRF_TRUSTED_ORIGINS = ['https://chefify-production.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = [
+    "https://chefify-production.up.railway.app"   
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
