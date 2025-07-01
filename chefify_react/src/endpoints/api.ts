cec29bc9626c6331bd45393914bc4d002949423a
Kevin <101434732+kevinhuytran1128@users.noreply.github.com>
2025-06-30 22:50:22 -0400

    update

--- chefify_react/src/endpoints/api.ts
+++ chefify_react/src/endpoints/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
-// const BASE_URL = import.meta.env.VITE_API_URL;
-const BASE_URL = "http://127.0.0.1:8000/api/";
+const BASE_URL = import.meta.env.VITE_API_URL;
+// const BASE_URL = "http://127.0.0.1:8000/api/";
 const CREATE_URL = "create/";
 const READ_URL = "read/";
 const UPDATE_URL = "update/";
