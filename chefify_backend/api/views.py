6c34f6dda9ad0544f2f70669bcd604ffbd82c8cb
Kevin <mudKIPZ1974@gmail.com>
2025-07-15 14:47:16 -0400

    update

--- chefify_backend/api/views.py
+++ chefify_backend/api/views.py
@@ -122,7 +122,6 @@ def googleLogin(request):
         profile.profilePictureUrl = photo_url
         profile.save()
 
-        
         # 3. Generate access/refresh tokens like your existing JWT login
         refresh = RefreshToken.for_user(user)
         access_token = str(refresh.access_token)
@@ -130,6 +129,10 @@ def googleLogin(request):
         res = Response({"success": True})
 
         # 4. Set them as cookies (match your existing logic)
+
+        res.delete_cookie("access_token", path="/")
+        res.delete_cookie("refresh_token", path="/")
+
         res.set_cookie(
             key="access_token",
             value=access_token,
--- chefify_react/vite.config.ts
+++ chefify_react/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
 import path from "path";
