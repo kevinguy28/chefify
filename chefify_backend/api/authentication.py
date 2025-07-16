e14af01da595a292bdc7950e7d467506e0103301
Kevin <101434732+kevinhuytran1128@users.noreply.github.com>
2025-07-16 14:32:29 -0400

    debug

--- chefify_backend/api/authentication.py
+++ chefify_backend/api/authentication.py
@@ -1,16 +1,16 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.exceptions import AuthenticationFailed
 
 class CookiesJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
-        access_token = request.COOKIES.get('access_token')
+        access_token = request.COOKIES.get("access_token")
         if not access_token:
-            return None
-
-        validated_token= self.get_validated_token(access_token)
+            return None  # No cookie means unauthenticated
 
         try:
+            validated_token = self.get_validated_token(access_token)
             user = self.get_user(validated_token)
-        except:
-            return None
-        
-        return(user, validated_token)
+            return (user, validated_token)
+        except Exception as e:
+            print("❌ Invalid JWT in cookie:", e)
+            raise AuthenticationFailed("Invalid or expired access token")
