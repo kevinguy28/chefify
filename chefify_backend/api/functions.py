a1fd660a1cc10076a8cdd47261375a47573355b0
Kevin <mudKIPZ1974@gmail.com>
2025-08-05 18:18:34 -0400

    update

--- chefify_backend/api/functions.py
+++ chefify_backend/api/functions.py
@@ -1,3 +1,11 @@
+"""
+functions.py
+
+Module where helper function(s) reside.
+
+"""
+
+
 def capitalize(sentence: str) -> str:
     """Capitalize sentence, return sentence."""
     return " ".join([word.capitalize() for word in sentence.split()])
